08add5e3e1da6f9920ee75c066d5fb1f181251a4
Rustin-Liu <rustin.liu@gmail.com>
2020-01-11 19:59:19 +0800

    feat(project-2): complete part 7

--- docs/project-2/README.md
+++ docs/project-2/README.md
@@ -15,10 +15,10 @@
 #### Q4: Where is the system performing buffering and where do you need buffering? 
 `I think I should have buffer when I read and write the log file.`
 
-### Q5: What is the impact of buffering on subsequent reads? 
+#### Q6: What is the impact of buffering on subsequent reads? 
 `I think the buffer may help us complete the index.`
 
-### Q5: When should you open and close file handles? For each command? For the lifetime of the KvStore?
+#### Q7: When should you open and close file handles? For each command? For the lifetime of the KvStore?
 `I think lifetime of the KvStore maybe a better choice.`
 
 ### Part 4: Reading from the log
@@ -27,3 +27,31 @@
 
 ### Part 6: Stateless vs. stateful KvStore 
 `My database is stateful database.`
+
+### Part 7: Compacting the log
+
+#### Q1: What is the naive solution? 
+`Read the all logs and remove it.`
+
+#### Q2: How much memory do you need?
+`The number of keys.`
+
+#### Q3: What is the minimum amount of copying necessary to compact the log?
+`The number of keys.`
+
+#### Q4: Can the compaction be done in-place?
+`No, I think I have to copy the logs.`
+
+#### Q4: How do you maintain data-integrity if compaction fails?
+`Maybe we can make a copy before we start compact.`
+
+
+
+
+
+
+
+
+
+
+
--- project-2/src/kv.rs
+++ project-2/src/kv.rs
@@ -11,6 +11,9 @@ use serde_json::Deserializer;
 use std::borrow::BorrowMut;
 use std::ops::Range;
 
+const COMPACTION_THRESHOLD: u64 = 1024 * 1024;
+const LOG_FILE_NAME: &str = "kvs.log";
+
 /// The `KvStore` stores string key/value pairs.
 ///
 /// Key/value pairs are persisted to disk in log files.
@@ -28,12 +31,17 @@ use std::ops::Range;
 /// # }
 /// ```
 pub struct KvStore {
+    // the current log file director path.
+    path: PathBuf,
     // reader of the current log.
     reader: BufReaderWithPos<File>,
     // writer of the current log.
     writer: BufWriterWithPos<File>,
     // the command position index.
     index: BTreeMap<String, CommandPos>,
+    // the number of bytes representing "stale" commands that could be
+    // deleted during a compaction.
+    need_compacted: u64,
 }
 
 impl KvStore {
@@ -47,20 +55,21 @@ impl KvStore {
     pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
         let path = path.into();
         fs::create_dir_all(&path)?;
-
-        let path = path.join("kvs.log");
-
-        let writer = new_log_file(&path)?;
+        let log_path = path.join(LOG_FILE_NAME);
 
         let mut index = BTreeMap::new();
+        let writer = new_log_file(&log_path)?;
+        let mut reader = BufReaderWithPos::new(File::open(log_path)?)?;
 
-        let mut reader = BufReaderWithPos::new(File::open(path)?)?;
+        let mut need_compacted = 0;
+        need_compacted += load(&mut reader, &mut index)?;
 
-        load(&mut reader, &mut index)?;
         Ok(KvStore {
+            path,
             reader,
             writer,
             index,
+            need_compacted,
         })
     }
 
@@ -77,7 +86,13 @@ impl KvStore {
         serde_json::to_writer(&mut self.writer, &cmd)?;
         self.writer.flush()?;
         if let Command::Set { key, .. } = cmd {
-            self.index.insert(key, (pos..self.writer.pos).into());
+            if let Some(old_cmd) = self.index.insert(key, (pos..self.writer.pos).into()) {
+                self.need_compacted += old_cmd.len;
+            }
+        }
+
+        if self.need_compacted > COMPACTION_THRESHOLD {
+            self.compact()?;
         }
         Ok(())
     }
@@ -117,12 +132,48 @@ impl KvStore {
             serde_json::to_writer(&mut self.writer, &cmd)?;
             self.writer.flush()?;
             if let Command::Remove { key } = cmd {
-                self.index.remove(&key).expect("key not found");
+                let old_cmd = self.index.remove(&key).expect("key not found");
+                self.need_compacted += old_cmd.len;
             }
             Ok(())
         } else {
             Err(KvsError::KeyNotFound)
         }
+    }
+
+    /// Clears stale entries in the log.
+    pub fn compact(&mut self) -> Result<()> {
+        let path = self.path.join(LOG_FILE_NAME.to_owned() + ".temp");
+        let mut temp_writer = new_log_file(&path)?;
+
+        let mut new_pos = 0;
+        for cmd_pos in &mut self.index.values_mut() {
+            let reader = self.reader.borrow_mut();
+            if reader.pos != cmd_pos.pos {
+                reader.seek(SeekFrom::Start(cmd_pos.pos))?;
+            }
+
+            let mut entry_reader = reader.take(cmd_pos.len);
+            let len = io::copy(&mut entry_reader, &mut temp_writer)?;
+            *cmd_pos = (new_pos..new_pos + len).into();
+            new_pos += len;
+        }
+        temp_writer.flush()?;
+
+        let old_file_path = &self.get_log_file_path()?;
+        fs::remove_file(old_file_path)?;
+        fs::rename(path, old_file_path)?;
+        self.need_compacted = 0;
+        // need reset writer.
+        self.writer = new_log_file(&old_file_path)?;
+        Ok(())
+    }
+
+    /// Get log file path.
+    ///
+    /// Returns the path of the log file.
+    fn get_log_file_path(&mut self) -> Result<PathBuf> {
+        Ok(self.path.join(LOG_FILE_NAME))
     }
 }
 
@@ -141,26 +192,33 @@ fn new_log_file(path: &Path) -> Result<BufWriterWithPos<File>> {
 }
 
 /// Load the whole log file and store value locations in the index map.
+///
+/// Returns how many bytes can be saved after a compaction.
 fn load(
     reader: &mut BufReaderWithPos<File>,
     index: &mut BTreeMap<String, CommandPos>,
-) -> Result<()> {
+) -> Result<u64> {
     let mut pos = reader.seek(SeekFrom::Start(0))?;
     let mut stream = Deserializer::from_reader(reader).into_iter::<Command>();
-
+    let mut need_compacted = 0;
     while let Some(cmd) = stream.next() {
         let new_pos = stream.byte_offset() as u64;
         match cmd? {
             Command::Set { key, .. } => {
-                index.insert(key, (pos..new_pos).into());
+                if let Some(old_cmd) = index.insert(key, (pos..new_pos).into()) {
+                    need_compacted += old_cmd.len;
+                }
             }
             Command::Remove { key } => {
-                index.remove(&key);
+                if let Some(old_cmd) = index.remove(&key) {
+                    need_compacted += old_cmd.len;
+                }
+                need_compacted += new_pos - pos;
             }
         }
         pos = new_pos;
     }
-    Ok(())
+    Ok(need_compacted)
 }
 
 /// Struct representing a command.
