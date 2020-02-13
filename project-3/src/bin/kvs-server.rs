23202eb9caf9fffc73399bead45993b9644ef558
Rustin-Liu <rustin.liu@gmail.com>
2020-02-13 21:26:39 +0800

    feat(project-3): complete part1 command line parsing
    pass client/server_cli_version tests
    
    Signed-off-by: Rustin-Liu <rustin.liu@gmail.com>

---
+++ project-3/src/bin/kvs-server.rs
@@ -0,0 +1,39 @@
+use clap::AppSettings;
+use kvs::Result;
+use std::net::SocketAddr;
+use std::process::exit;
+use structopt::StructOpt;
+
+const DEFAULT_LISTENING_ADDRESS: &str = "127.0.0.1:4000";
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "kvs-server")]
+struct Opt {
+    #[structopt(
+        long,
+        help = "Start the server and begin listening for the server address",
+        value_name = "IP:PORT",
+        default_value = "127.0.0.1:4000",
+        parse(try_from_str)
+    )]
+    addr: SocketAddr,
+    #[structopt(
+        long,
+        help = "Start with which store engine.",
+        value_name = "ENGINE-NAME",
+        default_value = "kvs"
+    )]
+    engine: String,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    if let Err(e) = run(opt) {
+        eprintln!("{}", e);
+        exit(1);
+    }
+}
+
+fn run(opt: Opt) -> Result<()> {
+    unimplemented!()
+}
