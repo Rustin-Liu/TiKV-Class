3fd3a6fe8ce9897882dc330dd7138ab01dc6c26d
Rustin-Liu <rustin.liu@gmail.com>
2020-02-27 23:56:38 +0800

    fix(project-4): add rayon sled bench test
    Signed-off-by: Rustin-Liu <rustin.liu@gmail.com>

--- project-4/benches/kvs_benchmark.rs
+++ project-4/benches/kvs_benchmark.rs
@@ -15,6 +15,7 @@ use tempfile::tempdir;
 
 const DEFAULT_LISTENING_ADDRESS: &str = "127.0.0.1:4000";
 const KVS: &str = "kvs";
+const SLED: &str = "sled";
 const SHARED_POOL: &str = "shared";
 const RAYON_POOL: &str = "rayon";
 
@@ -202,11 +203,105 @@ fn rayon_kvs_read_bench(c: &mut Criterion) {
     );
 }
 
+fn rayon_sled_write_bench(c: &mut Criterion) {
+    let thread_nums = vec![4, 8];
+    c.bench_function_over_inputs(
+        "rayon_sled_write",
+        |b, &num| {
+            let temp_dir = tempdir().unwrap();
+            let mut server = Command::cargo_bin("kvs-server").unwrap();
+            let mut child = server
+                .args(&[
+                    "--engine",
+                    SLED,
+                    "--addr",
+                    DEFAULT_LISTENING_ADDRESS,
+                    "--thread_pool",
+                    RAYON_POOL,
+                    "--thread_pool_size",
+                    &num.to_string(),
+                ])
+                .current_dir(&temp_dir)
+                .spawn()
+                .unwrap();
+            let (sender, receiver) = mpsc::sync_channel(0);
+            let handle = thread::spawn(move || {
+                let _ = receiver.recv(); // wait for main thread to finish
+                child.kill().expect("server exited before killed");
+            });
+            thread::sleep(Duration::from_secs(3));
+            b.iter(|| {
+                let mut client =
+                    KvsClient::init(SocketAddr::from_str(DEFAULT_LISTENING_ADDRESS).unwrap())
+                        .unwrap();
+                for i in 1..(1 << 10) {
+                    client
+                        .set(format!("key{}", i), "value".to_string())
+                        .unwrap();
+                }
+            });
+            sender.send(()).unwrap();
+            handle.join().unwrap();
+        },
+        thread_nums,
+    );
+}
+
+fn rayon_sled_read_bench(c: &mut Criterion) {
+    let thread_nums = vec![4, 8];
+    c.bench_function_over_inputs(
+        "rayon_sled_read",
+        |b, &num| {
+            let temp_dir = tempdir().unwrap();
+            let mut server = Command::cargo_bin("kvs-server").unwrap();
+            let mut child = server
+                .args(&[
+                    "--engine",
+                    SLED,
+                    "--addr",
+                    DEFAULT_LISTENING_ADDRESS,
+                    "--thread_pool",
+                    RAYON_POOL,
+                    "--thread_pool_size",
+                    &num.to_string(),
+                ])
+                .current_dir(&temp_dir)
+                .spawn()
+                .unwrap();
+            let (sender, receiver) = mpsc::sync_channel(0);
+            let handle = thread::spawn(move || {
+                let _ = receiver.recv(); // wait for main thread to finish
+                child.kill().expect("server exited before killed");
+            });
+            thread::sleep(Duration::from_secs(3));
+            let address = SocketAddr::from_str(DEFAULT_LISTENING_ADDRESS).unwrap();
+            let mut client = KvsClient::init(address).unwrap();
+            for i in 1..(1 << 10) {
+                client
+                    .set(format!("key{}", i), "value".to_string())
+                    .unwrap();
+            }
+            let mut rng = StdRng::seed_from_u64(64);
+            b.iter(|| {
+                let mut client = KvsClient::init(address).unwrap();
+                client
+                    .get(format!("key{}", rng.gen_range(0, 1 << 10)))
+                    .unwrap();
+            });
+            sender.send(()).unwrap();
+            handle.join().unwrap();
+        },
+        thread_nums,
+    );
+}
+
 criterion_group!(
     benches,
     shared_queue_kvs_write_bench,
     shared_queue_kvs_read_bench,
     rayon_kvs_write_bench,
     rayon_kvs_read_bench,
+    rayon_sled_write_bench,
+    rayon_sled_read_bench,
 );
 criterion_main!(benches);
