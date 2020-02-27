bc8dc591a6a281dcfc7bbb3442c75c01667fbd2a
Rustin-Liu <rustin.liu@gmail.com>
2020-02-27 08:49:03 +0800

    feat(project-4): add read bench test
    Signed-off-by: Rustin-Liu <rustin.liu@gmail.com>

--- project-4/benches/kvs_benchmark.rs
+++ project-4/benches/kvs_benchmark.rs
@@ -4,6 +4,7 @@ extern crate criterion;
 use assert_cmd::prelude::*;
 use criterion::Criterion;
 use kvs::KvsClient;
+use rand::prelude::*;
 use std::net::SocketAddr;
 use std::process::Command;
 use std::str::FromStr;
@@ -17,7 +18,7 @@ const KVS: &str = "kvs";
 const SHARED_POOL: &str = "shared";
 
 fn shared_queue_kvs_write_bench(c: &mut Criterion) {
-    let thread_nums = vec![4];
+    let thread_nums = vec![2, 4, 6, 8];
     c.bench_function_over_inputs(
         "shared_queue_kvs",
         |b, &num| {
@@ -47,7 +48,7 @@ fn shared_queue_kvs_write_bench(c: &mut Criterion) {
                 let mut client =
                     KvsClient::init(SocketAddr::from_str(DEFAULT_LISTENING_ADDRESS).unwrap())
                         .unwrap();
-                for i in 1..10 {
+                for i in 1..(1 << 10) {
                     client
                         .set(format!("key{}", i), "value".to_string())
                         .unwrap();
@@ -60,5 +61,57 @@ fn shared_queue_kvs_write_bench(c: &mut Criterion) {
     );
 }
 
-criterion_group!(benches, shared_queue_kvs_write_bench);
+fn shared_queue_kvs_read_bench(c: &mut Criterion) {
+    let thread_nums = vec![2, 4, 6, 8];
+    c.bench_function_over_inputs(
+        "shared_queue_kvs",
+        |b, &num| {
+            let temp_dir = tempdir().unwrap();
+            let mut server = Command::cargo_bin("kvs-server").unwrap();
+            let mut child = server
+                .args(&[
+                    "--engine",
+                    KVS,
+                    "--addr",
+                    DEFAULT_LISTENING_ADDRESS,
+                    "--thread_pool",
+                    SHARED_POOL,
+                    "--thread_pool_size",
+                    &num.to_string(),
+                ])
+                .current_dir(&temp_dir)
+                .spawn()
+                .unwrap();
+            let (sender, receiver) = mpsc::sync_channel(0);
+            let handle = thread::spawn(move || {
+                let _ = receiver.recv(); // wait for main thread to finish
+                child.kill().expect("server exited before killed");
+            });
+            thread::sleep(Duration::from_secs(1));
+            let address = SocketAddr::from_str(DEFAULT_LISTENING_ADDRESS).unwrap();
+            let mut client = KvsClient::init(address).unwrap();
+            for i in 1..(1 << 10) {
+                client
+                    .set(format!("key{}", i), "value".to_string())
+                    .unwrap();
+            }
+            let mut rng = StdRng::seed_from_u64(64);
+            b.iter(|| {
+                let mut client = KvsClient::init(address).unwrap();
+                for i in 1..(1 << 10) {
+                    client.get(format!("key{}", rng.gen_range(0, i))).unwrap();
+                }
+            });
+            sender.send(()).unwrap();
+            handle.join().unwrap();
+        },
+        thread_nums,
+    );
+}
+
+criterion_group!(
+    benches,
+    shared_queue_kvs_write_bench,
+    shared_queue_kvs_read_bench
+);
 criterion_main!(benches);
